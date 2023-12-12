77aa63e888e076fe38988a7b95d9abd9916f6e35
jeffersonbalde <jeffersonbalde8@gmail.com>
2023-12-13 01:36:12 +0800

    Add project files.

---
+++ CRUD APP/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace CRUD_APP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Name = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEDP = new System.Windows.Forms.TextBox();
+            this.txtCourse = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Name
+            // 
+            this.Name.AutoSize = true;
+            this.Name.Location = new System.Drawing.Point(12, 119);
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(47, 16);
+            this.Name.TabIndex = 0;
+            this.Name.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 172);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "EDP No.";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 226);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Course:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(98, 114);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(211, 22);
+            this.txtName.TabIndex = 3;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(15, 274);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(294, 45);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "ADD";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(975, 45);
+            this.panel1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(439, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(169, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "STUDENT INFORMATION";
+            // 
+            // txtEDP
+            // 
+            this.txtEDP.Location = new System.Drawing.Point(98, 167);
+            this.txtEDP.Name = "txtEDP";
+            this.txtEDP.Size = new System.Drawing.Size(211, 22);
+            this.txtEDP.TabIndex = 6;
+            // 
+            // txtCourse
+            // 
+            this.txtCourse.Location = new System.Drawing.Point(98, 221);
+            this.txtCourse.Name = "txtCourse";
+            this.txtCourse.Size = new System.Drawing.Size(211, 22);
+            this.txtCourse.TabIndex = 7;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(15, 341);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(294, 45);
+            this.btnUpdate.TabIndex = 8;
+            this.btnUpdate.Text = "UPDATE";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(15, 408);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(294, 45);
+            this.btnDelete.TabIndex = 9;
+            this.btnDelete.Text = "DELETE";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(345, 60);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(613, 393);
+            this.dataGridView1.TabIndex = 10;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(98, 60);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(211, 22);
+            this.txtID.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 65);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 16);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "ID: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(975, 476);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.txtCourse);
+            this.Controls.Add(this.txtEDP);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Name);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Name;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtEDP;
+        private System.Windows.Forms.TextBox txtCourse;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ CRUD APP/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Text;
+using System.Data.OleDb;
+
+namespace CRUD_APP
+{
+    public partial class Form1 : Form
+    {
+
+        OleDbConnection cn = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\jeffe\OneDrive\Documents\School.accdb");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void LoadData()
+        {
+            try
+            {
+
+                cn.Open();
+                string query = "SELECT * FROM student";
+                OleDbCommand cm = new OleDbCommand(query, cn);
+                cm.ExecuteNonQuery();
+                DataTable dt = new DataTable();
+                OleDbDataAdapter dp = new OleDbDataAdapter(cm);
+                dp.Fill(dt);
+                dataGridView1.DataSource = dt;
+                cn.Close();
+
+            }
+            catch (Exception ex)
+            {
+                cn.Close();
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void ClearInput()
+        {
+            txtID.Text = "";
+            txtName.Text = "";
+            txtEDP.Text = "";
+            txtCourse.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PrivateFontCollection pfc = new PrivateFontCollection();
+            pfc.AddFontFile(@"C:\Users\jeffe\OneDrive\Documents\inter font\inter\inter-Regular.ttf");
+            foreach (Control c in this.Controls)
+            {
+                c.Font = new Font(pfc.Families[0], 10, FontStyle.Regular);
+            }
+
+            LoadData();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                cn.Open();
+                string query = "INSERT INTO student(StudentName, StudentEDP, StudentCourse) VALUES(@name, @edp, @course)";
+                OleDbCommand cm = new OleDbCommand(query, cn);
+                cm.Parameters.AddWithValue("@name", txtName.Text);
+                cm.Parameters.AddWithValue("@edp", txtEDP.Text);
+                cm.Parameters.AddWithValue("@course", txtCourse.Text);
+                cm.ExecuteNonQuery();
+                cn.Close();
+                MessageBox.Show("Student added successfully", "CRUD APP", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                LoadData();
+                ClearInput();
+            }
+            catch (Exception ex)
+            {
+                cn.Close();
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                cn.Open();
+                string query = "UPDATE student SET StudentName = @name, StudentEDP = @edp, StudentCourse = @course WHERE ID = @id";
+                OleDbCommand cm = new OleDbCommand(query, cn);
+                cm.Parameters.AddWithValue("@name", txtName.Text);
+                cm.Parameters.AddWithValue("@edp", txtEDP.Text);
+                cm.Parameters.AddWithValue("@course", txtCourse.Text);
+                cm.Parameters.AddWithValue("@id", txtID.Text);
+                cm.ExecuteNonQuery();
+                cn.Close();
+                MessageBox.Show("Student updated!", "CRUD APP", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                LoadData();
+                ClearInput();
+
+            }
+            catch (Exception ex)
+            {
+                cn.Close();
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                cn.Open();
+                string query = "DELETE * FROM student WHERE ID=@id";
+                OleDbCommand cm = new OleDbCommand(query, cn);
+                cm.Parameters.AddWithValue("@id", int.Parse(txtID.Text));
+                cm.ExecuteNonQuery();
+                cn.Close();
+                MessageBox.Show("Student deleted!", "CRUD APP", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                LoadData();
+                ClearInput();
+            }
+            catch (Exception ex)
+            {
+                cn.Close();
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {  
+                txtID.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+                txtName.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+                txtEDP.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+                txtCourse.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();   
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
